466861edd099b5e9be5609765ab141aed0ad2561
Rafay <abdulrafaykhan857@gmail.com>
2022-11-17 17:05:20 +0500

    bugs fixes

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "220px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
-
-
-  window.onscroll = function() {scrollFunction()};
-
-  function scrollFunction() {
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-      document.getElementById("navTop").style.padding= "12px 15px";
-      } else {
-      document.getElementById("navTop").style.padding = "10px 0px 0px 0px";
-      }
-
-
-if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-document.getElementById("navTop").style.backgroundColor= "black";
-} else {
-document.getElementById("navTop").style.backgroundColor = "inherit";
+  document.getElementById("mySidenav").style.width = "220px";
 }
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navTop").style.padding = "12px 15px";
+  } else {
+    document.getElementById("navTop").style.padding = "10px 0px 0px 0px";
+  }
+
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navTop").style.backgroundColor = "#1a1a1a";
+  } else {
+    document.getElementById("navTop").style.backgroundColor = "inherit";
+  }
 }
